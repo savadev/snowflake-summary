039fbc028d664fe561b1255795a94dc7709b55f7
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-09-11 15:43:13 -0300

    updated course review

--- COURSE-REVIEW.sql
+++ COURSE-REVIEW.sql
@@ -5940,4 +5940,91 @@ CREATE OR REPLACE PROCEDURE COLUMN_FILL_RATE_BIND_VAR(TABLE_NAME VARCHAR)
         $$;
 
 
-CALL COLUMN_FILL_RATE_BIND_VAR('DEMO_DB.PUBLIC.CUSTOMER')
+CALL COLUMN_FILL_RATE_BIND_VAR('DEMO_DB.PUBLIC.CUSTOMER');
+
+
+
+
+
+-- Example of procedure - Show column fill rate for a given table, in tabular format
+
+CREATE OR REPLACE PROCEDURE COLUMN_FILL_RATE(TABLE_NAME varchar)
+  RETURNS VARIANT NOT NULL
+  LANGUAGE JAVASCRIPT
+  EXECUTE AS CALLER -- Caller Procedure
+  as     
+  $$  
+    var my_sql_command = "select COUNT(*) CNT from "+ TABLE_NAME +";"
+    var statement1 = snowflake.createStatement( {sqlText: my_sql_command} );
+    var result_set1 = statement1.execute();
+    result_set1.next();
+    
+    var my_sql_command2 = "select * from "+ TABLE_NAME +" LIMIT 10 ;"
+    var statement2 = snowflake.createStatement( {sqlText: my_sql_command2} );
+    var result_set2 = statement2.execute();
+    
+    var cnt = result_set1.getColumnValue(1);
+    var array_of_rows = [];
+    
+    
+    var row_num = 0;
+    row_as_json = {};
+    
+    var column_name;
+    
+      while (result_set2.next())  {
+       -- // Put each row in a variable of type JSON.
+         
+       -- // For each column in the row...
+        for (var col_num = 0; 
+        col_num < result_set2.getColumnCount(); -- we can run this because we called "result_set3.next()" in the while loop.
+        col_num = col_num + 1) {
+          var col_name =result_set2.getColumnName(col_num+1);
+          
+         var my_sql_command3 = "select round(count(*)/"+cnt+",2) RW_CNT from "+ TABLE_NAME +" where "+col_name+" IS NOT NULL;"
+         
+        -- // return cnt;
+         var statement3 = snowflake.createStatement( {sqlText: my_sql_command3} );
+      
+           result_set3 = statement3.execute();
+          
+           result_set3.next();
+           var col_value = result_set3.getColumnValue(1); -- we can run this because we called "result_set3.next()" in the previous line.
+          
+           row_as_json = { ColumnName : col_name ,column_value : col_value}
+          
+           array_of_rows.push(row_as_json)
+          }
+          
+        }   
+        
+      table_as_json = { "key1" : array_of_rows };
+      
+         for (var col_num = 0; col_num < result_set2.getColumnCount(); col_num = col_num + 1) {
+         
+         var my_sql_command4 = "insert into Table_fill_rate values (:1 , :2 ) "
+      
+         var statement4 = snowflake.createStatement( {sqlText: my_sql_command4,binds: [table_as_json.key1[col_num].ColumnName,table_as_json.key1[col_num].column_value]} );
+      
+         statement4.execute();
+         
+         }
+ 
+  return table_as_json; 
+  $$
+  ;
+
+
+
+
+-- Output of above procedure:
+
+COLUMN_NAME	FILL_RATE
+C_CUSTKEY	1
+C_NAME	1
+C_ADDRESS	1
+C_NATIONKEY	1
+C_PHONE	1
+C_ACCTBAL	1
+C_MKTSEGMENT	1
+C_COMMENT	1
